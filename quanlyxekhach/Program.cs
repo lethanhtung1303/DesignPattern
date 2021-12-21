f6e9e758ff09ddd118d8bb123ec74c8bcbc0fe1f
XuanThinh <thinhgood9@gmail.com>
2021-12-21 23:20:05 +0700

    change open form first

--- quanlyxekhach/Program.cs
+++ quanlyxekhach/Program.cs
@@ -16,8 +16,8 @@ namespace quanlyxekhach
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Login());
-            Application.Run(new ManageCustomer());
+            Application.Run(new Login());
+            //Application.Run(new ManageCustomer());
             //Application.Run(new ManageEmployee());
         }
     }
